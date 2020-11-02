79ee2f1b6240163fb1079b88c42fe60ba01df0ba
Sumit Garg <sumit.garg@linaro.org>
2020-11-02 17:14:45 +0530

    arm64: ipi_nmi: Add support for NMI backtrace
    Enable NMI backtrace support on arm64 using IPI turned as an NMI
    leveraging pseudo NMIs support. It is now possible for users to get a
    backtrace of a CPU stuck in hard-lockup using magic SYSRQ.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>

--- arch/arm64/kernel/ipi_nmi.c
+++ arch/arm64/kernel/ipi_nmi.c
@@ -8,6 +8,7 @@
 
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/nmi.h>
 #include <linux/smp.h>
 
 #include <asm/nmi.h>
@@ -31,11 +32,22 @@ void arm64_send_nmi(cpumask_t *mask)
 	__ipi_send_mask(ipi_nmi_desc, mask);
 }
 
+void arch_trigger_cpumask_backtrace(const cpumask_t *mask, bool exclude_self)
+{
+	if (!ipi_nmi_desc)
+		return;
+
+	nmi_trigger_cpumask_backtrace(mask, exclude_self, arm64_send_nmi);
+}
+
 static irqreturn_t ipi_nmi_handler(int irq, void *data)
 {
-	/* nop, NMI handlers for special features can be added here. */
+	irqreturn_t ret = IRQ_NONE;
 
-	return IRQ_NONE;
+	if (nmi_cpu_backtrace(get_irq_regs()))
+		ret = IRQ_HANDLED;
+
+	return ret;
 }
 
 void dynamic_ipi_setup(int cpu)
