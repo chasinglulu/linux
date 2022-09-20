3ee9b07b2e06197e316c5954b4f5700ee9609564
charleye <wangkart@aliyun.com>
2022-09-20 16:00:39 +0800

    arm64: Add Hobot SoC support

---
+++ arch/arm64/include/asm/serial.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Horizon Robotics Ltd.
+ *
+ */
+#ifndef ARCH_ARM64_INCLUDE_ASM_SERIAL_H_
+#define ARCH_ARM64_INCLUDE_ASM_SERIAL_H_
+/*
+ * This is used for 16550-compatible UARTs
+ */
+#define BASE_BAUD (192000000/16)
+
+#endif
