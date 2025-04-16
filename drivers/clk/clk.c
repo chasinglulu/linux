968c0deca27d21cac0ede560262a9cafa0f40059
chasinglulu <wangkartx@gmail.com>
2025-04-16 13:45:24 +0800

    drivers: clk: Format clk_summary output

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -3116,27 +3116,27 @@ static void clk_summary_show_one(struct seq_file *s, struct clk_core *c,
 {
 	int phase;
 
-	seq_printf(s, "%*s%-*s %7d %8d %8d %11lu %10lu ",
+	seq_printf(s, "%*s%-*s %4d %7d %10d %12lu %5lu ",
 		   level * 3 + 1, "",
-		   30 - level * 3, c->name,
+		   64 - level * 3, c->name,
 		   c->enable_count, c->prepare_count, c->protect_count,
 		   clk_core_get_rate_recalc(c),
 		   clk_core_get_accuracy_recalc(c));
 
 	phase = clk_core_get_phase(c);
 	if (phase >= 0)
-		seq_printf(s, "%5d", phase);
+		seq_printf(s, "%6d", phase);
 	else
-		seq_puts(s, "-----");
+		seq_puts(s, "------");
 
-	seq_printf(s, " %6d", clk_core_get_scaled_duty_cycle(c, 100000));
+	seq_printf(s, " %8d", clk_core_get_scaled_duty_cycle(c, 100000));
 
 	if (c->ops->is_enabled)
-		seq_printf(s, " %9c\n", clk_core_is_enabled(c) ? 'Y' : 'N');
+		seq_printf(s, " %6c\n", clk_core_is_enabled(c) ? 'Y' : 'N');
 	else if (!c->ops->enable)
-		seq_printf(s, " %9c\n", 'Y');
+		seq_printf(s, " %6c\n", 'Y');
 	else
-		seq_printf(s, " %9c\n", '?');
+		seq_printf(s, " %6c\n", '?');
 }
 
 static void clk_summary_show_subtree(struct seq_file *s, struct clk_core *c,
@@ -3157,9 +3157,9 @@ static int clk_summary_show(struct seq_file *s, void *data)
 	struct clk_core *c;
 	struct hlist_head **lists = (struct hlist_head **)s->private;
 
-	seq_puts(s, "                                 enable  prepare  protect                                duty  hardware\n");
-	seq_puts(s, "   clock                          count    count    count        rate   accuracy phase  cycle    enable\n");
-	seq_puts(s, "-------------------------------------------------------------------------------------------------------\n");
+	seq_puts(s, "                                                                  enable   prepare  protect                             duty  hardware\n");
+	seq_puts(s, "   clock                                                           count     count    count     rate   accuracy phase  cycle    enable\n");
+	seq_puts(s, "---------------------------------------------------------------------------------------------------------------------------------------\n");
 
 	clk_prepare_lock();
 
